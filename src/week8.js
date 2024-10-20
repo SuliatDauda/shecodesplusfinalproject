6eac4d82ce876c08e22b6b8559dd1711f73d1921
SuliatDauda <suliatdauda1@gmail.com>
2024-10-20 20:51:25 +0100

    Added ApiUrl and Apikey to the js file

--- src/week8.js
+++ src/week8.js
@@ -1,8 +1,19 @@
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "905390deebf4616a1a1o9903d8ct5d65";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityBox = document.querySelector("#weather-city");
   cityBox.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormBox = document.querySelector("#search-form");
