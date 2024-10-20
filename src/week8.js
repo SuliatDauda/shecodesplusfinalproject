5f75be69562e908220e4f7ecd7912bad119428a5
SuliatDauda <suliatdauda1@gmail.com>
2024-10-20 20:14:20 +0100

    Added id to the HTML and innerHTML to the  h1 in js file

---
+++ src/week8.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityBox = document.querySelector("#weather-city");
+  cityBox.innerHTML = searchInput.value;
+}
+
+let searchFormBox = document.querySelector("#search-form");
+searchFormBox.addEventListener("submit", handleSearchSubmit);
